cf3cd056f825741675fab753d73b2e4c867335f2
frkolyan <frkolyan@mail.ru>
2016-10-01 13:14:13 +0300

    packets projects added

---
+++ NetworkMonitor1/PerformanceCounter/PerfCounter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerformanceCounter
+{
+    public class PerfCounter
+    {
+        protected System.Diagnostics.PerformanceCounter cpuCounter; 
+        protected System.Diagnostics.PerformanceCounter ramCounter;
+
+        public PerfCounter()
+        {
+            cpuCounter = new System.Diagnostics.PerformanceCounter();
+
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+
+            ramCounter = new System.Diagnostics.PerformanceCounter("Memory", "Available MBytes"); 
+        }
+
+        public string getCurrentCpuUsage(){ 
+            return cpuCounter.NextValue()+"%"; 
+        } 
+ 
+        public string getAvailableRAM(){
+            return ramCounter.NextValue() + "Mb"; 
+        }
+    }
+}
